9360b6c9a8e9c4e05fee0839c9c0c40c7c1c70d7
wk82 <weikaiyc0@163.com>
2022-04-05 21:36:39 +0800

    pratice

---
+++ ex5.py
@@ -0,0 +1,16 @@
+name = "zed"
+age = "35"
+height = "170"
+weight = "180"
+eyes = "blue"
+teeth = "white"
+hair = "black"
+
+print(f"let's talk about {name}")
+print(f"he's {height} inches tall")
+print(f"he's {weight} pounds heavy")
+print("actually that's not too heavy")
+print(f"got {eyes} eyes and {hair} hair ")
+print(f"teeth are {teeth} on the office")
+total = age + height + weight
+print(f"if i add {age}, {height}, and {weight} i get {total}")
