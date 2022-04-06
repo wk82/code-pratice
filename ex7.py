7caedca212dd46e07c21517ccd6d7c0134028bae
wk82 <weikaiyc0@163.com>
2022-04-06 22:52:30 +0800

    m

---
+++ ex7.py
@@ -0,0 +1,15 @@
+print("mary had a little lamb")
+print("its fleece was white as {}.".format('snow'))
+print("and everywhere that mary went")
+print("." * 10)
+
+end1 = "c"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "b"
+
+print(end1 + end2 + end3 + end4 + end5, end='\n')
+print("uyhggh")
