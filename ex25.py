2b9596824c391acaec03176ca7db2d16e5b05ff5
wk82 <weikaiyc0@163.com>
2022-04-19 22:44:09 +0800

    m

---
+++ ex25.py
@@ -0,0 +1,26 @@
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats:
+    print("too many cats! the world is doomed!")
+
+if people > cats:
+    print("not many cats!")
+
+if people < dogs:
+    print("world")
+
+if people > dogs:
+    print("dry")
+
+dogs += 5
+
+if people >= dogs:
+    print("people")
+
+if people <= dogs:
+    print("less than")
+
+if people == dogs:
+    print("people are dogs")
