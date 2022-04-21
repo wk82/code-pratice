20d3bb45e8e3b24bba8f965a7a05a0f2fe8dd878
wk82 <weikaiyc0@163.com>
2022-04-21 23:33:30 +0800

    m

---
+++ ex26.py
@@ -0,0 +1,36 @@
+print("""you enter a dark room with two doors.
+do you go through door #1 or door #2?""")
+
+door = input("> ")
+
+if door == "1":
+    print("there is a giant bear here eating a cheese cake.")
+    print("what do you do?")
+    print("1. take the cake.")
+    print("2.scream at the bear.")
+
+    bear = input("> ")
+
+    if bear == "1":
+        print("the bear eats your face off. good job!")
+    elif bear == "2":
+        print("the bear eats your legs off. good job!")
+    else:
+        print(f"well, doing {bear} is probably better.")
+        print("bear runs away.")
+elif door =="2":
+    print("you stare into the endless abyss at cthulhu's retina")
+    print("1. blueberries")
+    print("2. yellow")
+    print("3. understanding")
+
+    insanity = input("> ")
+    if insanity == "1" or insanity == "2":
+        print("your body survives powered by a mind of jello.")
+        print("good job")
+    else:
+        print("the insanity rots your eyes into a pool of muck.")
+        print("good job")
+
+else:
+    print("you stumble around and fal on a knife an die. good job!")
