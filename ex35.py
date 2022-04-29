a93ca09b23ba043a6e576f4cccfb09ae4fd373bb
wk82 <weikaiyc0@163.com>
2022-04-30 01:11:39 +0800

    m

---
+++ ex35.py
@@ -0,0 +1,65 @@
+class animal(object):
+    pass
+##??
+class dog(animal):
+    def __init__(self, name):
+        ##??
+        self.name = name
+
+##??
+class cat(animal):
+    def __init__(self, name):
+        ##??
+        self.name = name
+
+##??
+class person(object):
+
+    def __init__(self, name):
+        ##??
+        self.name = name
+        self.pet = None
+
+##??
+class employee(person):
+    def __init__(self, name, salary):
+        ##??
+        super(employee, self).__init__(name)
+        ##??
+        self.salary = salary
+
+##??
+class salmon(fish):
+    pass
+##??
+
+class halibut(fish):
+    pass
+
+## rover is-a dog
+rover = dog("rover")
+
+##??
+satan = cat("satan")
+
+##??
+mary = person("mary")
+
+##??
+mary.pet = satan
+
+##??
+frank = employee("frank", 120000)
+
+##??
+frank.pet = rover
+
+
+##??
+flipper = fish()
+
+##??
+crouse = salmon()
+
+##??
+harry = halibut()
